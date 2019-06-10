50234442e0082bb61f400365a97484a12f4af1c0
ivanfursa <retired.inhausted@gmail.com>
2019-06-10 14:44:03 +0100

    Added comments

--- browser/control.js
+++ browser/control.js
@@ -2,11 +2,12 @@
 var socket = io.connect("http://localhost:3000");
 
 $(function () {
-    // If something is inputted, show button
-    var input = $("#url");  //if by tag name - don't add hashtag
+    // DOM elements
+    var input = $("#url");
     var button = $("#buttonBlock");
     var video = $("#video");
 
+    // If something is inputted, show the URL-request button
     input.on("input", function() {
         if (input.val() == "") {
             button.hide();
@@ -15,6 +16,7 @@ $(function () {
         }
     });
 
+    // Send "pause" and "play" events to browser
     video.on("play", function(){
         socket.emit('playVideo');
     });
@@ -22,27 +24,36 @@ $(function () {
         socket.emit('pauseVideo');
     });
 
+    // Process the inputted video URL and send to server if valid
     button.click(function() {
         var url = String(input.val()).trim();
         if (url.indexOf("https://www.youtube.com/watch?v=") == 0) {
             url = url.replace("watch?v=", "embed/");
             video.css("visibility", "visible");
-            video.attr("src", url);
             input.attr("placeholder", "Paste a Youtube Link");
+
+            // Send URL to the server
+            var request = new XMLHttpRequest();
+            request.onreadystatechange = function() {
+                if (this.readyState == 4 && this.status == 200) {
+                    socket.emit("linkSent", url);   // Send URL for streaming
+                }
+            };
+            request.open("GET", "http://localhost:3000/videoTest", true)    // Send URL for testing
+            request.send()
         } else {
             input.attr("placeholder", "Wrong link");
         }
+        // Clean-up
         input.val("");
         button.hide();
-        socket.emit('linkSent', url);
     });
 
-    // Listening to events
-    socket.on('pauseVideo', function(){
+    // Listening to users' actions
+    socket.on("pauseVideo", function(){
         video.pause();
     });
-
-    socket.on('playVideo', function(){
+    socket.on("playVideo", function(){
         video.play();
     });
 });
--- server/server.js
+++ server/server.js
@@ -1,21 +1,39 @@
 // npm run dev - to run the code via nodemon
+
+// Loading modules
 const express = require('express')
 const socket = require('socket.io')
 const fs = require('fs')
 const path = require('path')
 const ytdl = require('ytdl-core')
 
+// Instantiating Express JS
 const app = express()
 
+// Server setup
+var server = app.listen(3000, function () {
+  console.log('Listening on port 3000!')
+})
+
+// Send the HTML file to the root of the server
 app.get('/', function(req, res) {
   res.sendFile('../browser/webLayout.html')
 })
 
-var videoURL = 'https://www.youtube.com/watch?v=r1d-t3EqbDk';
-var test = ytdl(videoURL);
-var fileSize;
-test.on('progress', (n1, n2, size) => {
-  fileSize = size;
+var videoURL = 'https://www.youtube.com/watch?v=r1d-t3EqbDk' // Default video
+var fileSize
+
+app.get('/videoTest', function (req, res) {
+  // Load the video to learn its file size
+  var test = ytdl(videoURL, { range: { start: 0, end: 600 } });
+  var responded = false;
+  test.on('progress', (n1, n2, size) => {
+    if (!responded) {
+      fileSize = size;
+      res.send();
+    }
+    responded = true;
+  })
 })
 
 app.get('/video', function (req, res) {
@@ -36,13 +54,8 @@ app.get('/video', function (req, res) {
     file.pipe(res)
 })
 
-var server = app.listen(3000, function () {
-  console.log('Listening on port 3000!')
-})
-
 // Socket back-end setup
 var io = socket(server);
-
 io.on("connection", function(socket){
     socket.on('linkSent', function(url) {videoURL = url});
     socket.on('playVideo', function() {socket.broadcast.emit('playVideo')});
